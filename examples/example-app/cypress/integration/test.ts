c823a8817191ba268ac47711c5c25a6f870104bb
manuelJung <manu.jung@relaxcommerce.com>
2020-06-02 15:29:38 +0200

    updated sctructure

--- examples/example-app/cypress/integration/test.ts
+++ examples/example-app/cypress/integration/test.ts
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
-/// <reference path="../support/firescout-types.ts" />
+/// <reference path="../support/firescout.d.ts" />
+
+export const foo = ''
 
 context('Actions', () => {
   it('test', () => {
-    cy.component('organisms/Wishlist')
-      .collection('ColorFilter')
-      .shouldHaveState('selected')
-      .handle('clear-button')
-      .click()
+    // cy.component('organisms/Wishlist')
+    //   .collection('ColorFilter')
+    //   .shouldHaveState('selected')
+    //   .handle('clear-button')
+    //   .click()
 
     cy.module('cart')
       .fn('fetch')
