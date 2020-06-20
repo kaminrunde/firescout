92fe69dc2d43a63dd63fef7f76a6e4e6e4016577
manuelJung <manu.jung@relaxcommerce.com>
2020-06-21 01:20:00 +0200

    updated reporting

--- packages/cypress-firescout/lib/build-docs/reporter.js
+++ packages/cypress-firescout/lib/build-docs/reporter.js
@@ -8,12 +8,62 @@ var codes = {
     HANDLE_WITHOUT_PARENT: function (item) { return item.file; },
     STATE_WITHOUT_PARENT: function (item) { return item.file; },
     COLLECTION_WITHOUT_PARENT: function (item) { return item.file; },
+    HANDLE_HAS_NO_DOCS: function (file) { return file; },
+    STATE_HAS_NO_DOCS: function (file) { return file; },
 };
 function report(code, ctx) {
     console.log(code, codes[code](ctx));
 }
 exports.report = report;
 function validate(input) {
+    /**
+     * all sets have the same signature: "[ctx]-[collection]?-[handle]":file
+     * the collection is optional or can be multiple
+     */
+    var treeCtx = new Map();
+    var treeCollections = new Map();
+    var treeHandles = new Map();
+    var treeStates = new Map();
+    var docsCtx = new Map();
+    var docsCollections = new Map();
+    var docsHandles = new Map();
+    var docsStates = new Map();
+    for (var _i = 0, _a = input.tree; _i < _a.length; _i++) {
+        var tree = _a[_i];
+        treeCtx.set(tree.context, tree.file);
+        for (var _b = 0, _c = tree.handles; _b < _c.length; _b++) {
+            var handle = _c[_b];
+            treeHandles.set(tree.context + '-' + handle.name, handle.file);
+        }
+        for (var _d = 0, _e = tree.states; _d < _e.length; _d++) {
+            var state = _e[_d];
+            treeStates.set(tree.context + '-' + state.name, state.file);
+        }
+    }
+    for (var ctx in input.docs) {
+        var doc = input.docs[ctx];
+        docsCtx.set(doc.context, doc.file);
+        for (var _f = 0, _g = doc.handles.bullets; _f < _g.length; _f++) {
+            var handle = _g[_f];
+            docsHandles.set(doc.context + '-' + handle.name, doc.file);
+        }
+        for (var _h = 0, _j = doc.states.bullets; _h < _j.length; _h++) {
+            var state = _j[_h];
+            docsStates.set(doc.context + '-' + state.name, doc.file);
+        }
+    }
+    for (var handle in treeHandles) {
+        if (!docsHandles.has(handle)) { }
+    }
+    for (var state in treeStates) {
+        if (!docsStates.has(state)) { }
+    }
+    for (var handle in docsHandles) {
+        if (!treeHandles.has(handle)) { }
+    }
+    for (var state in docsStates) {
+        if (!treeStates.has(state)) { }
+    }
     return input;
 }
 exports.validate = validate;
--- packages/cypress-firescout/src/build-docs/reporter.ts
+++ packages/cypress-firescout/src/build-docs/reporter.ts
@@ -13,11 +13,15 @@ const codes = {
   HANDLE_WITHOUT_PARENT: (item:RawItem) => item.file,
   STATE_WITHOUT_PARENT: (item:RawItem) => item.file,
   COLLECTION_WITHOUT_PARENT: (item:RawItem) => item.file,
+  HANDLE_HAS_NO_DOCS: (file:string) => file,
+  STATE_HAS_NO_DOCS: (file:string) => file,
 }
 
 export function report(code:'HANDLE_WITHOUT_PARENT', item:RawItem):void
 export function report(code:'STATE_WITHOUT_PARENT', item:RawItem):void
 export function report(code:'COLLECTION_WITHOUT_PARENT', item:RawItem):void
+export function report(code:'HANDLE_HAS_NO_DOCS', item:RawItem):void
+export function report(code:'STATE_HAS_NO_DOCS', item:RawItem):void
 export function report (code:keyof typeof codes, ctx:any) {
   console.log(code, codes[code](ctx))
 }
@@ -29,5 +33,46 @@ type Input = {
 }
 
 export function validate (input:Input):Input {
+  /**
+   * all sets have the same signature: "[ctx]-[collection]?-[handle]":file
+   * the collection is optional or can be multiple
+   */
+  const treeCtx = new Map<string,string>()
+  const treeCollections = new Map<string,string>()
+  const treeHandles = new Map<string, string>()
+  const treeStates = new Map<string, string>()
+  const docsCtx = new Map<string,string>()
+  const docsCollections = new Map<string,string>()
+  const docsHandles = new Map<string, string>()
+  const docsStates = new Map<string, string>()
+  for(let tree of input.tree) {
+    treeCtx.set(tree.context, tree.file)
+    for(let handle of tree.handles) treeHandles.set(tree.context + '-' + handle.name, handle.file)
+    for(let state of tree.states) treeStates.set(tree.context + '-' + state.name, state.file)
+  }
+
+  for(let ctx in input.docs) {
+    const doc = input.docs[ctx]
+    docsCtx.set(doc.context,doc.file)
+    for(let handle of doc.handles.bullets) docsHandles.set(doc.context + '-' + handle.name, doc.file)
+    for(let state of doc.states.bullets) docsStates.set(doc.context + '-' + state.name, doc.file)
+  }
+
+  for(let handle in treeHandles) {
+    if(!docsHandles.has(handle)) {}
+  }
+
+  for(let state in treeStates) {
+    if(!docsStates.has(state)) {}
+  }
+
+  for(let handle in docsHandles) {
+    if(!treeHandles.has(handle)) {}
+  }
+
+  for(let state in docsStates) {
+    if(!treeStates.has(state)) {}
+  }
+
   return input
 }
