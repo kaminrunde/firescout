027f5f722a5223d3ce0df6b64058af16bf3e6d94
manuelJung <manu.jung@relaxcommerce.com>
2020-06-21 13:36:38 +0200

    setup testing

--- packages/cypress-firescout/lib/build-docs/__TEST__/searchWithNode.test.js
+++ packages/cypress-firescout/lib/build-docs/__TEST__/searchWithNode.test.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var e = expect;
 function sum(a, b) {
     return a + b;
 }
 test('adds 1 + 2 to equal 3', function () {
-    // expect(sum(1, 2)).toBe(3);
+    e(sum(1, 2)).toBe(3);
 });
--- packages/cypress-firescout/src/build-docs/__TEST__/searchWithNode.test.ts
+++ packages/cypress-firescout/src/build-docs/__TEST__/searchWithNode.test.ts
@@ -1,9 +1,11 @@
 import searchWidthNode from '../searchWithNode'
 
+const e:any = expect
+
 function sum(a:any, b:any) {
   return a + b;
 }
 
 test('adds 1 + 2 to equal 3', () => {
-  // expect(sum(1, 2)).toBe(3);
+  e(sum(1, 2)).toBe(3)
 });
