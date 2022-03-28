9e79537b16fcd5634cc4d4f94c9c58e3442d0834
paul <mail@paulgrotzke.com>
2022-03-28 19:31:25 +0200

    setup createFileContent.ts

---
+++ packages/react-firescout/src/createFileContent.ts
@@ -0,0 +1,11 @@
+import { Tree } from "@kaminrunde/firescout-utils/lib/build-docs/createCommandTree";
+import { Docs } from "@kaminrunde/firescout-utils/lib/build-docs/createDocs";
+import { ModuleTree } from "@kaminrunde/firescout-utils/lib/build-docs/createModuleTree";
+
+export default function createFileContent(
+  tree: Tree[],
+  docs: Docs,
+  modules: ModuleTree[]
+) {
+  return ``;
+}
