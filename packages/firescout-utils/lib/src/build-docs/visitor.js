753fcfb0776e5c64a95935123bce04466e9da9fb
paul <mail@paulgrotzke.com>
2023-03-02 20:10:56 +0100

    add handle for undefined node

--- packages/firescout-utils/lib/src/build-docs/visitor.js
+++ packages/firescout-utils/lib/src/build-docs/visitor.js
@@ -20,31 +20,33 @@ var Visitor = /** @class */ (function () {
         var result = '';
         for (var _i = 0, nodes_1 = nodes; _i < nodes_1.length; _i++) {
             var node = nodes_1[_i];
-            switch (node.type) {
-                case 'bold':
-                case 'strike':
-                case 'italic':
-                    result += node.style + Visitor.getMd(node.block) + node.style;
-                    break;
-                case 'text':
-                case 'border':
-                case 'break':
-                    result += node.text;
-                    break;
-                case 'codeBlock':
-                    result += node.indent + node.syntax + node.code + node.indent;
-                    break;
-                case 'codeSpan':
-                    result += '`' + node.code + '`';
-                    break;
-                // case 'image': result += node.
-                // case 'link': result += node.
-                // case 'linkDef':
-                // case 'list': result += node.
-                // case 'quote':
-                case 'title':
-                    result += Array(node.rank).fill('#').join('') + ' ' + Visitor.getMd(node.block);
-                    break;
+            if (node) {
+                switch (node.type) {
+                    case 'bold':
+                    case 'strike':
+                    case 'italic':
+                        result += node.style + Visitor.getMd(node.block) + node.style;
+                        break;
+                    case 'text':
+                    case 'border':
+                    case 'break':
+                        result += node.text;
+                        break;
+                    case 'codeBlock':
+                        result += node.indent + node.syntax + node.code + node.indent;
+                        break;
+                    case 'codeSpan':
+                        result += '`' + node.code + '`';
+                        break;
+                    // case 'image': result += node.
+                    // case 'link': result += node.
+                    // case 'linkDef':
+                    // case 'list': result += node.
+                    // case 'quote':
+                    case 'title':
+                        result += Array(node.rank).fill('#').join('') + ' ' + Visitor.getMd(node.block);
+                        break;
+                }
             }
         }
         return result;
@@ -57,15 +59,18 @@ var Visitor = /** @class */ (function () {
     Visitor.prototype.fit = function (rows) {
         var _this = this;
         var recursive = function (node) {
-            if (_this.listeners[node.type]) {
-                _this.listeners[node.type].forEach(function (l) { return l(node); });
-            }
-            // @ts-ignore
-            if (node.block)
-                for (var _i = 0, _a = node.block; _i < _a.length; _i++) {
-                    var block = _a[_i];
-                    recursive(block);
+            if (node) {
+                if (_this.listeners[node.type]) {
+                    _this.listeners[node.type].forEach(function (l) { return l(node); });
                 }
+                // @ts-ignore
+                if (node.block)
+                    // @ts-ignore
+                    for (var _i = 0, _a = node.block; _i < _a.length; _i++) {
+                        var block = _a[_i];
+                        recursive(block);
+                    }
+            }
         };
         for (var _i = 0, rows_1 = rows; _i < rows_1.length; _i++) {
             var block = rows_1[_i];
--- packages/firescout-utils/src/build-docs/visitor.ts
+++ packages/firescout-utils/src/build-docs/visitor.ts
@@ -19,31 +19,33 @@ export default class Visitor {
   static getMd(nodes: md.Node[]) {
     let result = ''
     for (let node of nodes) {
-      switch (node.type) {
-        case 'bold':
-        case 'strike':
-        case 'italic':
-          result += node.style + Visitor.getMd(node.block) + node.style
-          break
-        case 'text':
-        case 'border':
-        case 'break':
-          result += node.text
-          break
-        case 'codeBlock':
-          result += node.indent + node.syntax + node.code + node.indent
-          break
-        case 'codeSpan':
-          result += '`' + node.code + '`'
-          break
-        // case 'image': result += node.
-        // case 'link': result += node.
-        // case 'linkDef':
-        // case 'list': result += node.
-        // case 'quote':
-        case 'title':
-          result += Array(node.rank).fill('#').join('') + ' ' + Visitor.getMd(node.block)
-          break
+      if (node) {
+        switch (node.type) {
+          case 'bold':
+          case 'strike':
+          case 'italic':
+            result += node.style + Visitor.getMd(node.block) + node.style
+            break
+          case 'text':
+          case 'border':
+          case 'break':
+            result += node.text
+            break
+          case 'codeBlock':
+            result += node.indent + node.syntax + node.code + node.indent
+            break
+          case 'codeSpan':
+            result += '`' + node.code + '`'
+            break
+          // case 'image': result += node.
+          // case 'link': result += node.
+          // case 'linkDef':
+          // case 'list': result += node.
+          // case 'quote':
+          case 'title':
+            result += Array(node.rank).fill('#').join('') + ' ' + Visitor.getMd(node.block)
+            break
+        }
       }
     }
     return result
@@ -56,14 +58,17 @@ export default class Visitor {
 
   fit(rows: md.Node[]) {
     const recursive = (node: md.Node) => {
-      if (this.listeners[node.type]) {
-        this.listeners[node.type].forEach((l: any) => l(node))
-      }
-      // @ts-ignore
-      if (node.block)
-        for (let block of node.block) {
-          recursive(block)
+      if (node) {
+        if (this.listeners[node.type]) {
+          this.listeners[node.type].forEach((l: any) => l(node))
         }
+        // @ts-ignore
+        if (node.block)
+          // @ts-ignore
+          for (let block of node.block) {
+            recursive(block)
+          }
+      }
     }
 
     for (let block of rows) recursive(block)
